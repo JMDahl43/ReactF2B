b5c3f81fcc9a2ad3f697bd7d55f5855b1b2af4f8
Julie Dahl <jmdahl43@gmail.com>
2022-04-27 14:52:58 -0500

    Add Form with Submit Button functionality.

---
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/*@todo - rating select component*/}
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            name=''
+            id=''
+            value={text}
+            placeholder='Write a Review'
+          />
+          <Button type='submit'>Send</Button>
+        </div>
+      </form>
+    </Card>
+  )
+}
+export default FeedbackForm
