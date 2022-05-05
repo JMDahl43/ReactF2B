14496df6d3581212cf8c79a33c4611ade86d0780
JDahl <jmdahl43@gmail.com>
2022-05-04 22:55:33 -0500

    Fix About page layout issue.

--- feedback-app/src/pages/AboutPage.jsx
+++ feedback-app/src/pages/AboutPage.jsx
@@ -1,10 +1,10 @@
-import Card from '../components/shared/Card'
 import { Link } from 'react-router-dom'
+import Card from '../components/shared/Card'
 
 function AboutPage() {
   return (
     <Card>
-      <div className='about-link'>
+      <div className='about'>
         <h1>About this Project</h1>
         <p> React App to lead feedback for a product or services.</p>
         <p>Version: 1.0.0</p>
