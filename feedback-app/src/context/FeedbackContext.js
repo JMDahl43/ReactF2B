59705a5eb348d0b40a665aa385bdd7afb0d5f672
JDahl <jmdahl43@gmail.com>
2022-05-22 14:41:19 -0500

    Modify Context, Add Feedback, to add data to mock dB.

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -18,7 +17,7 @@ export const FeedbackProvider = ({ children }) => {
 
 // Fetch Feedback
 const fectchFeedback = async () => {
-  const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+  const response = await fetch(`/feedback?_sort=id&_order=desc`)
   const data = await response.json()
 
   setFeedback(data)
@@ -27,10 +26,19 @@ const fectchFeedback = async () => {
 
 
   // Add Feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-    console.log(newFeedback)
+  const addFeedback = async(newFeedback) => {
+    const response =  await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
+    
   }
 
   // Delete Feedback
