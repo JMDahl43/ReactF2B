8114cbe009257e34460a8eb0e01dc869ae59d7de
JDahl <jmdahl43@gmail.com>
2022-06-09 16:20:42 -0500

    Uploading images to Firebase updates.

--- house-marketplace/src/pages/CreateListing.jsx
+++ house-marketplace/src/pages/CreateListing.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage'
+import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
-import { async } from '@firebase/util'
+import { v4 as uuidv4 } from 'uuid'
 
 function CreateListing() {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true)
@@ -91,19 +98,74 @@ function CreateListing() {
       geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
       geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
 
-      location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0].formatted_address
+      location =
+        data.status === 'ZERO_RESULTS'
+          ? undefined
+          : data.results[0].formatted_address
 
-      if(location === undefined || location.includes('undefined')) {
-          setLoading(false)
-          toast.error('Please enter a valid address.')
-          return
+      if (location === undefined || location.includes('undefined')) {
+        setLoading(false)
+        toast.error('Please enter a valid address.')
+        return
       }
-
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
       location = address
     }
+
+    // Store images in firebase - run func on each image
+    const storeImage = async (image) => {
+      // Return new promise
+      return new Promise((resolve, reject) => {
+        const storage = getStorage()
+        const fileName = `${auth.currentUser.uid}-${image.name}-&{uuidv4()}`
+
+        const storageRef = ref(storage, 'images/' + fileName)
+
+        // Upload Task
+        const uploadTask = uploadBytesResumable(storageRef, image)
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            console.log('Upload is ' + progress + '% done')
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused')
+                break
+              case 'running':
+                console.log('Upload is running')
+                break
+              default:
+                break
+            }
+          },
+          (error) => {
+            // Handle unsuccessful uploads
+            reject(error)
+          },
+          () => {
+            // Handle successful uploads on complete
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL)
+            })
+          }
+        )
+      })
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false)
+      toast.error('Images no uploaded.')
+    })
+
+    console.log(imgUrls)
+
     setLoading(false)
   }
 
