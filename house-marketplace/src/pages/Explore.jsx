99f3ff03f339a2a32a8db74a5c3cadabf8a11804
JDahl <jmdahl43@gmail.com>
2022-06-16 12:19:43 -0500

    Add Slider component for Explore.

--- house-marketplace/src/pages/Explore.jsx
+++ house-marketplace/src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import Slider from '../components/Slider'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
@@ -9,7 +10,8 @@ function Explore() {
         <p className='pageHeader'>Explore</p>
       </header>
       <main>
-        {/* Slider */}
+        
+        <Slider />
 
         <p className='exploreCateoryHeading'>Categories</p>
         <div className='exploreCategories'>
